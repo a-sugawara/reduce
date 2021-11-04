11fe57b0ece1dec641922b715519ed87fe5f4845
Alfredo Sugawara <nicora221@gmail.com>
2021-11-04 16:26:35 -0400

    setup phase complete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,10 +4,38 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
+if (process.env.NODE_ENV !== 'production') {
+    router.get('/api/csrf/restore', (req, res) => {
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      return res.json({});
+    });
+}
+
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    // Serve the frontend's index.html file at the root route
+    router.get('/', (req, res) => {
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      return res.sendFile(
+        path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+      );
+});
+
+
+router.use(express.static(path.resolve("../frontend/build")));
+
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
+
+router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
 
 module.exports = router;
